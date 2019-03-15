72a3a7c111cdb1f767ce24baa14c0bfd110532ea
Gabriel Queiroz <gabrieljvnq@gmail.com>
2019-03-15 13:37:17 -0300

    Starting to use a more DOM-like model, no more streaming.

--- go/xml_tree.go
+++ go/xml_tree.go
@@ -7,76 +7,197 @@ import (
 	"strings"
 )
 
-type labelConfig struct {
+type LabelConfig struct {
 	Sec string
 	Cls string
 	Sub string
 	Toc string
 }
 
-type xmlTreePath []*xmlTreeNode
+type xmlTreePath []*XMLTreeNode
 
-type xmlTreeNode struct {
+type XMLTreeNode struct {
 	Token          xml.Token
-	Parent         *xmlTreeNode
-	Children       []*xmlTreeNode
-	NthChild       int
-	NthChildOfElem int
+	Parent         *XMLTreeNode
+	Children       []*XMLTreeNode
+	LabelConfig 	LabelConfig
 }
 
-func (this *xmlTreeNode) AddChild(token xml.Token) *xmlTreeNode {
-	if this.Children == nil {
-		this.Children = make([]*xmlTreeNode, 0)
-	}
-	new_child := new(xmlTreeNode)
-	new_child.Parent = this
-	new_child.Token = xml.CopyToken(token)
-	new_child.NthChild = len(this.Children) + 1
-	new_child.NthChildOfElem = 1
+func (this *XMLTreeNode) Encode(out io.Writer) error {
+	return this.encode(out, 0)
+}
 
-	// Find NthChildOfElem
-	for _, child := range this.Children {
-		if token_same_element(new_child.Token, child.Token) {
-			new_child.NthChildOfElem++
+func (this *XMLTreeNode) encode(out io.Writer, depth int) error {
+	var err error
+	buf := new(bytes.Buffer)
+	has_to_end_tag := false
+	end_tag := new(bytes.Buffer)
+
+	// Ensure proper indentation
+	for i:=0; i < depth; i++ {
+		buf.WriteString("\t")
+		end_tag.WriteString("\t")
+	}
+
+	// Encode this token (unless we are root)
+	if this.Parent != nil {
+		switch tk := this.Token.(type) {
+			case xml.StartElement:
+				buf.WriteString("<")
+				buf.WriteString(name2string(tk.Name))
+				for _, attr := range tk.Attr {
+					buf.WriteString(" ")
+					buf.WriteString(name2string(attr.Name))
+					buf.WriteString(`="`)
+					xml.EscapeText(buf, attr.Value)
+					buf.WriteString(`"`)
+				}
+				// Make self closing tag if possible
+				if len(this.Children) == 0 {
+					buf_str += "/>"
+					buf.WriteString(`/>`)
+				} else {
+					buf.WriteString(`>`)
+					has_to_end_tag = true
+					end_tag.WriteString(`<`)
+					end_tag.WriteString(name2string(tk.Name))
+					end_tag.WriteString(`/>`)
+				}
+			case xml.CharData:
+				err = xml.EscapeText(buf, []byte(tk))
+				if err != nil {
+					return err
+				}
+			case xml.Comment:
+				if bytes.Contains(tk, `-->`) {
+					return errors.New("xml: EncodeToken of Comment containing --> marker")
+				}
+				buf.WriteString(`<!--`)
+				buf.Write([]byte(tk))
+				buf.WriteString(`-->`)
+			case xml.ProcInst:
+				buf.WriteString(`<!`)
+				buf.Write([]byte(tk.Target))
+				if len(tk.Inst) {
+					buf.WriteString(` `)
+					buf.Write([]byte(tk.Inst))
+				}
+				buf.WriteString(`?>`)
+			case xml.Directive:
+				buf.WriteString(`<!`)
+				buf.Write([]byte(tk))
+				buf.WriteString(`>`)
+			default:
+				return errors.New("xml: EncodeToken of invalid token type")
+			}
+		}
+
+		// Output
+		buf.WriteString("\n")
+		err = out.Write(buf)
+		if err != nil {
+			return err
 		}
 	}
 
-	if _, ok := token.(xml.StartElement); ok {
-		Debugln("AddChild:", token2string(token), fmt.Sprintf("(%d|%d)", new_child.NthChild, new_child.NthChildOfElem))
+	// Encode children
+	depth++
+	for _, child := this.Children {
+		err = child.encode(out, depth)
+		if err != nil {
+			return err
+		}
 	}
-	this.Children = append(this.Children, new_child)
+	depth--
 
+	// Finalize tag if needed (also ensure we are not root)
+	end_tag.WriteString("\n")
+	if has_to_end_tag && this.Parent != nil {
+		err = out.Write([]byte(end_tag))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (this *XMLTreeNode) NthChild() int {
+	for i, child := range this.Parent.Children {
+		if child == this {
+			return i
+		}
+	}
+	panic("token not contained in parent")
+}
+
+func (this *XMLTreeNode) NthChildOfElem() int {
+	ans := 0
+	for _, child := range this.Parent.Children {
+		if token_same_element(this, child) {
+			ans++
+		}
+	}
+	return ans
+}
+
+func (this *XMLTreeNode) new_child(token xml.Token) *XMLTreeNode {
+	if this.Children == nil {
+		this.Children = make([]*XMLTreeNode, 0)
+	}
+	new_child := new(XMLTreeNode)
+	new_child.Parent = this
+	new_child.Token = xml.CopyToken(token)
+}
+
+func (this *XMLTreeNode) Tag(token xml.Token) *XMLTreeNode {
+	switch tk := this.Token.(type) {
+	case xml.StartElement:
+		return name2string(tk.Name)
+	default:
+		return ""
+	}
+}
+
+func (this *XMLTreeNode) AddChild(token xml.Token) *XMLTreeNode {
+	new_child := this.new_child(token)
+	this.Children = append(this.Children, new_child)
 	return new_child
 }
 
-func (this *xmlTreeNode) Replace(token xml.Token) {
+func (this *XMLTreeNode) Replace(token xml.Token) {
 	this.Token = token
-	this.NthChild = 1
-	this.NthChildOfElem = 1
-	for _, ptr := range this.Parent.Children {
-		if ptr == this {
-			break
-		}
-		this.NthChild++
-		if token_same_element(this.Token, ptr.Token) {
-			this.NthChildOfElem++
-		}
-	}
 }
 
-func (this *xmlTreeNode) PathToHere() xmlTreePath {
-	buf := make([]*xmlTreeNode, 0)
+func (this *XMLTreeNode) insert_at(index int, token xml.Token) *XMLTreeNode {
+	new_child := this.new_child(token)
+	this.Children = this.Children[0:len(this.Children)+1]
+	copy(slice[index+1:], slice[index:])
+	this.Children[index] = new_child
+	return new_child
+}
+
+func (this *XMLTreeNode) InsertBefore(token xml.Token) *XMLTreeNode {
+	return this.Parent.insert_at(this.NthChild(), token)
+}
+
+func (this *XMLTreeNode) InsertAfter(token xml.Token) *XMLTreeNode {
+	return this.Parent.insert_at(this.NthChild()+1, token)
+}
+
+func (this *XMLTreeNode) PathToHere() xmlTreePath {
+	buf := make([]*XMLTreeNode, 0)
 	buf = xmlTreePath(this.pathToHere(buf))
 	// Reverse list
 	my_len := len(buf)
-	ans := make([]*xmlTreeNode, my_len)
+	ans := make([]*XMLTreeNode, my_len)
 	for i, _ := range buf {
 		ans[my_len-i-1] = buf[i]
 	}
 	return xmlTreePath(ans)
 }
 
-func (this *xmlTreeNode) pathToHere(buf []*xmlTreeNode) []*xmlTreeNode {
+func (this *XMLTreeNode) pathToHere(buf []*XMLTreeNode) []*XMLTreeNode {
 	if this.Token == nil {
 		return buf
 	}
@@ -152,7 +273,7 @@ func (this xmlTreePath) LexId() string {
 	return ans
 }
 
-func (this xmlTreePath) Label(cls_counter int, cfg labelConfig) string {
+func (this xmlTreePath) Label(cls_counter int, cfg LabelConfig) string {
 	ans := ""
 	last_tag := ""
 	title := ""
@@ -200,4 +321,35 @@ func (this xmlTreePath) Label(cls_counter int, cfg labelConfig) string {
 	default:
 		return ans
 	}
+}
+
+func StreamXML2Tree(input io.Reader, trim bool) (error, *XMLTreeNode) {
+	// Prepare
+	root := new(XMLTreeNode)
+	cursor := root
+	decoder := xml.NewDecoder(input)
+
+	// Parse
+	for {
+		token, err := decoder.RawToken()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err, root
+		}
+		if trim {
+			token = sanitize_char_data(token)
+		}
+
+		switch tk := token.(type) {
+		case xml.StartElement:
+			cursor = cursor.AddChild(tk)
+		case xml.EndElement:
+			cursor = cursor.Parent
+		default:
+			cursor.AddChild(tk)
+		}
+	}
+	return root
 }
